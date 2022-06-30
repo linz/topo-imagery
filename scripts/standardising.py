c93dd5811293b37621a82396cd315efd0c8e943f
Paul Fouquet <pfouquet@linz.govt.nz>
2022-07-01 11:42:21 +1200

    feat: run standardising step on a list of files (TDE-412)

--- scripts/standardising.py
+++ scripts/standardising.py
@@ -12,61 +12,62 @@ parser.add_argument("--destination", dest="destination", required=True)
 arguments = parser.parse_args()
 source = arguments.source
 destination = arguments.destination
-# TODO if destination needs write permission we have to handle this
 
 get_log().info("standardising", source=source, destination=destination)
-
-src_bucket_name, src_file_path = parse_path(source)
 dst_bucket_name, dst_path = parse_path(destination)
-get_log().debug("source", bucket=src_bucket_name, file_path=src_file_path)
 get_log().debug("destination", bucket=dst_bucket_name, file_path=dst_path)
 dst_bucket = get_bucket(dst_bucket_name)
 
+file_list = [source]
+
 with tempfile.TemporaryDirectory() as tmp_dir:
-    standardized_file_name = f"standardized_{os.path.basename(src_file_path)}"
-    tmp_file_path = os.path.join(tmp_dir, standardized_file_name)
-    src_gdal_path = source.replace("s3://", "/vsis3/")
+    for file in file_list:
+        src_bucket_name, src_file_path = parse_path(source)
+        get_log().debug("processing_file", bucket=src_bucket_name, file_path=src_file_path)
+        standardized_file_name = f"standardized_{os.path.basename(src_file_path)}"
+        tmp_file_path = os.path.join(tmp_dir, standardized_file_name)
+        src_gdal_path = source.replace("s3://", "/vsis3/")
 
-    # Set the credentials for GDAL to be able to read the source file
-    credentials = get_credentials(src_bucket_name)
-    gdal_env = os.environ.copy()
-    gdal_env["AWS_ACCESS_KEY_ID"] = credentials.access_key
-    gdal_env["AWS_SECRET_ACCESS_KEY"] = credentials.secret_key
-    gdal_env["AWS_SESSION_TOKEN"] = credentials.token
+        # Set the credentials for GDAL to be able to read the source file
+        credentials = get_credentials(src_bucket_name)
+        gdal_env = os.environ.copy()
+        gdal_env["AWS_ACCESS_KEY_ID"] = credentials.access_key
+        gdal_env["AWS_SECRET_ACCESS_KEY"] = credentials.secret_key
+        gdal_env["AWS_SESSION_TOKEN"] = credentials.token
 
-    # Run GDAL to standardized the file
-    get_log().debug("run_gdal_translate", src=src_gdal_path, output=tmp_file_path)
-    command = [
-        "gdal_translate",
-        "-q",
-        "-scale",
-        "0",
-        "255",
-        "0",
-        "254",
-        "-a_srs",
-        "EPSG:2193",
-        "-a_nodata",
-        "255",
-        "-b",
-        "1",
-        "-b",
-        "2",
-        "-b",
-        "3",
-        "-co",
-        "compress=lzw",
-        src_gdal_path,
-        tmp_file_path,
-    ]
-    try:
-        proc = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=gdal_env, check=True)
-    except subprocess.CalledProcessError as cpe:
-        get_log().error("run_gdal_translate_failed", command=" ".join(command))
-        raise cpe
-    get_log().debug("run_gdal_translate_succeded", command=" ".join(command))
+        # Run GDAL to standardized the file
+        get_log().debug("run_gdal_translate", src=src_gdal_path, output=tmp_file_path)
+        command = [
+            "gdal_translate",
+            "-q",
+            "-scale",
+            "0",
+            "255",
+            "0",
+            "254",
+            "-a_srs",
+            "EPSG:2193",
+            "-a_nodata",
+            "255",
+            "-b",
+            "1",
+            "-b",
+            "2",
+            "-b",
+            "3",
+            "-co",
+            "compress=lzw",
+            src_gdal_path,
+            tmp_file_path,
+        ]
+        try:
+            proc = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=gdal_env, check=True)
+        except subprocess.CalledProcessError as cpe:
+            get_log().error("run_gdal_translate_failed", command=" ".join(command))
+            raise cpe
+        get_log().debug("run_gdal_translate_succeded", command=" ".join(command))
 
-    # Upload the standardized file to destination
-    dst_file_path = os.path.join(dst_path, standardized_file_name).strip("/")
-    get_log().debug("upload_file", path=dst_file_path)
-    dst_bucket.upload_file(tmp_file_path, dst_file_path)
+        # Upload the standardized file to destination
+        dst_file_path = os.path.join(dst_path, standardized_file_name).strip("/")
+        get_log().debug("upload_file", path=dst_file_path)
+        dst_bucket.upload_file(tmp_file_path, dst_file_path)
