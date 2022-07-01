c342a8849aa19e9dd15447ab8a4044949a3e0d9c
Megan Davidson <mdavidson@linz.govt.nz>
2022-07-01 15:50:34 +1200

    chore: formatting

--- scripts/tests/source_list_conversion_test.py
+++ scripts/tests/source_list_conversion_test.py
@@ -3,9 +3,9 @@ from source_formatter import format_source_from_basemaps_cli
 
 def test_format_source_from_basemaps_cli_file() -> None:
     """Based on output from basemaps cli
-    example: --source "[\"s3://test/image_one.tiff\", \"s3://test/image_two.tiff\"]"    
+    example: --source "[\"s3://test/image_one.tiff\", \"s3://test/image_two.tiff\"]"
     """
-    source = ["[\"s3://test/image_one.tiff\", \"s3://test/image_two.tiff\"]"]
+    source = ['["s3://test/image_one.tiff", "s3://test/image_two.tiff"]']
     file_list = format_source_from_basemaps_cli(source[0])
     assert isinstance(file_list, list)
     assert len(file_list) == 2
