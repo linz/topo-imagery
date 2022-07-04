7bca72544ca3112d231810e55fb82a7787e6724b
Megan Davidson <mdavidson@linz.govt.nz>
2022-07-04 12:57:10 +1200

    chore: black

--- scripts/format_source.py
+++ scripts/format_source.py
@@ -3,7 +3,7 @@ from typing import List, cast
 
 
 def format_source(source: List[str]) -> List[str]:
-    """ If source from basemaps cli it needs has a string which contains a list that needs to be split
+    """If source from basemaps cli it needs has a string which contains a list that needs to be split
     example: ["[\"s3://test/image_one.tiff, \"s3://test/image_two.tiff\"]"]
     """
     if len(source) == 1 and source[0].startswith("["):
--- scripts/tests/format_source_test.py
+++ scripts/tests/format_source_test.py
@@ -5,24 +5,26 @@ def test_format_source_from_basemaps_cli_file() -> None:
     """Based on output from basemaps cli
     example: --source "[\"s3://test/image_one.tiff\", \"s3://test/image_two.tiff\"]"
     """
+    # fmt: off
     source = ["[\"s3://test/image_one.tiff\", \"s3://test/image_two.tiff\"]"]
+    # fmt: on
     file_list = format_source(source)
     assert isinstance(file_list, list)
     assert len(file_list) == 2
     assert file_list[0] == "s3://test/image_one.tiff"
 
+
 def test_format_source_single_input() -> None:
-    """example: --source s3://test/image_one.tiff
-    """
+    """example: --source s3://test/image_one.tiff"""
     source = ["s3://test/image_one.tiff"]
     file_list = format_source(source)
     assert isinstance(file_list, list)
     assert len(file_list) == 1
     assert file_list[0] == "s3://test/image_one.tiff"
 
+
 def test_format_source_multiple_inputs() -> None:
-    """example: --source s3://test/image_one.tiff s3://test/image_two.tiff
-    """
+    """example: --source s3://test/image_one.tiff s3://test/image_two.tiff"""
     source = ["s3://test/image_one.tiff", "s3://test/image_two.tiff"]
     file_list = format_source(source)
     assert isinstance(file_list, list)
