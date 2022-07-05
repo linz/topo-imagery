f7c02d7c444f1f1efa57271bab883bbad5625342
Megan Davidson <mdavidson@linz.govt.nz>
2022-07-06 11:31:25 +1200

    fix: correct comment

--- scripts/format_source.py
+++ scripts/format_source.py
@@ -3,7 +3,7 @@ from typing import List, cast
 
 
 def format_source(source: List[str]) -> List[str]:
-    """If source from basemaps cli it has a string which contains a list that needs to be split
+    """Due to Argo constraints if using the basemaps cli the source has a string that contains a list that needs to be split.
     example: ["[\"s3://test/image_one.tiff, \"s3://test/image_two.tiff\"]"]
     """
     if len(source) == 1 and source[0].startswith("["):
