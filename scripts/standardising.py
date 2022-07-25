86b8c3ae49de908a16e7e8907bd78729b354326f
paulfouquet <86932794+paulfouquet@users.noreply.github.com>
2022-07-26 08:14:49 +1200

    fix: keep temporary files so they are pushed to the artifact bucket (#66)

--- scripts/standardising.py
+++ scripts/standardising.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-import tempfile
 
 from aws_helper import parse_path
 from file_helper import get_file_name_from_path
@@ -18,48 +17,47 @@ get_log().info("standardising", source=source)
 gdal_env = os.environ.copy()
 
 for file in source:
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        src_bucket_name, src_file_path = parse_path(file)
-        standardized_file_name = f"standardized_{get_file_name_from_path(src_file_path)}"
-        tmp_file_path = os.path.join(tmp_dir, standardized_file_name)
+    src_bucket_name, src_file_path = parse_path(file)
+    standardized_file_name = f"standardized_{get_file_name_from_path(src_file_path)}"
+    tmp_file_path = os.path.join("/tmp/", standardized_file_name)
 
-        command = [
-            "gdal_translate",
-            "-q",
-            "-scale",
-            "0",
-            "255",
-            "0",
-            "254",
-            "-a_srs",
-            "EPSG:2193",
-            "-a_nodata",
-            "255",
-            "-b",
-            "1",
-            "-b",
-            "2",
-            "-b",
-            "3",
-            "-of",
-            "COG",
-            "-co",
-            "compress=lzw",
-            "-co",
-            "num_threads=all_cpus",
-            "-co",
-            "predictor=2",
-            "-co",
-            "overview_compress=webp",
-            "-co",
-            "bigtiff=yes",
-            "-co",
-            "overview_resampling=lanczos",
-            "-co",
-            "blocksize=512",
-            "-co",
-            "overview_quality=90",
-            "-co",
-            "sparse_ok=true",
-        ]
-        run_gdal(command, input_file=file, output_file=tmp_file_path)
+    command = [
+        "gdal_translate",
+        "-q",
+        "-scale",
+        "0",
+        "255",
+        "0",
+        "254",
+        "-a_srs",
+        "EPSG:2193",
+        "-a_nodata",
+        "255",
+        "-b",
+        "1",
+        "-b",
+        "2",
+        "-b",
+        "3",
+        "-of",
+        "COG",
+        "-co",
+        "compress=lzw",
+        "-co",
+        "num_threads=all_cpus",
+        "-co",
+        "predictor=2",
+        "-co",
+        "overview_compress=webp",
+        "-co",
+        "bigtiff=yes",
+        "-co",
+        "overview_resampling=lanczos",
+        "-co",
+        "blocksize=512",
+        "-co",
+        "overview_quality=90",
+        "-co",
+        "sparse_ok=true",
+    ]
+    run_gdal(command, input_file=file, output_file=tmp_file_path)
