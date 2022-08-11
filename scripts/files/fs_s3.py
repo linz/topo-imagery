af33e3279e989fda15f1d53d073a98eba091fba1
Paul Fouquet <pfouquet@linz.govt.nz>
2022-08-11 14:43:05 +1200

    fix: s3 write and read error handling

--- scripts/files/fs_s3.py
+++ scripts/files/fs_s3.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Dict
 
 import boto3
 import botocore
@@ -11,8 +11,11 @@ if TYPE_CHECKING:
 else:
     S3ServiceResource = object
 
+# s3_session = [{"bucket", boto3.Session}]
+s3_sessions: Dict[str, S3ServiceResource] = {}
 
-def get_auth_s3_session(bucket_name: str) -> S3ServiceResource:
+
+def _get_s3_resource(bucket_name: str) -> S3ServiceResource:
     """Return a boto3 S3 Resource with the AWS credentials for a bucket.
 
     Args:
@@ -21,12 +24,17 @@ def get_auth_s3_session(bucket_name: str) -> S3ServiceResource:
     Returns:
         S3ServiceResource: The boto3 S3 Resource.
     """
-    credentials = get_credentials(bucket_name)
-    session = boto3.Session(
-        aws_access_key_id=credentials.access_key,
-        aws_secret_access_key=credentials.secret_key,
-        aws_session_token=credentials.token,
-    )
+    session = s3_sessions.get(bucket_name, None)
+    if session is None:
+        # TODO implement refreshable session TDE-235
+        credentials = get_credentials(bucket_name)
+        session = boto3.Session(
+            aws_access_key_id=credentials.access_key,
+            aws_secret_access_key=credentials.secret_key,
+            aws_session_token=credentials.token,
+        )
+    s3_sessions[bucket_name] = session
+
     s3: S3ServiceResource = session.resource("s3")
     return s3
 
@@ -44,23 +52,24 @@ def write(destination: str, source: bytes, needs_credentials: bool = False) -> N
         raise Exception("The 'source' is None.")
     s3_path = parse_path(destination)
     key = s3_path.key
-    s3 = boto3.resource("s3")
-    try:
-        if needs_credentials:
-            s3 = get_auth_s3_session(s3_path.bucket)
 
+    if needs_credentials:
+        s3 = _get_s3_resource(s3_path.bucket)
+    else:
+        s3 = boto3.resource("s3")
+
+    try:
         s3_object = s3.Object(s3_path.bucket, key)
-        response = s3_object.put(Body=source)
-        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
-            get_log().debug("write_s3_success", path=destination, response=response)
-        else:
-            get_log().error("write_s3_error", path=destination, response=response)
+        s3_object.put(Body=source)
+        # TODO add the duration
+        get_log().debug("write_s3_success", path=destination)
     except botocore.exceptions.ClientError as ce:
-        if not needs_credentials:
-            get_log().debug("write_s3_needs_credentials", path=destination)
+        # Boto3 Resources exceptions are not exposed like for Clients
+        if not needs_credentials and ce.response["Error"]["Code"] == "AccessDenied":
             write(destination, source, True)
-        else:
-            get_log().error("write_s3_error", path=destination, error=f"Unable to write the file: {ce}")
+
+        get_log().error("write_s3_error", path=destination, error=f"Unable to write the file: {ce}")
+        raise ce
 
 
 def read(path: str, needs_credentials: bool = False) -> bytes:
@@ -79,23 +88,21 @@ def read(path: str, needs_credentials: bool = False) -> bytes:
 
     try:
         if needs_credentials:
-            s3 = get_auth_s3_session(s3_path.bucket)
+            s3 = _get_s3_resource(s3_path.bucket)
 
         s3_object = s3.Object(s3_path.bucket, key)
         file: bytes = s3_object.get()["Body"].read()
     except botocore.exceptions.ClientError as ce:
-        if ce.response["Error"]["Code"] == "NoSuchBucket":
-            get_log().error("read_s3_bucket_not_found", path=path, error=f"The specified bucket does not seem to exist: {ce}")
-            raise ce
-        if ce.response["Error"]["Code"] == "NoSuchKey":
-            get_log().error("read_s3_file_not_found", path=path, error=f"The specified file does not seem to exist: {ce}")
-            raise ce
-        if not needs_credentials:
+        if not needs_credentials and ce.response["Error"]["Code"] == "AccessDenied":
             get_log().debug("read_s3_needs_credentials", path=path)
             return read(path, True)
+        if ce.response["Error"]["Code"] == "NoSuchBucket":
+            get_log().error("read_s3_bucket_not_found", path=path, error=f"The specified bucket does not seem to exist: {ce}")
+        if ce.response["Error"]["Code"] == "NoSuchKey":
+            get_log().error("read_s3_file_not_found", path=path, error=f"The specified file does not seem to exist: {ce}")
 
         get_log().error("read_s3_error", path=path, error=f"Unable to read the file: {ce}")
         raise ce
-
+    # TODO add the duration
     get_log().debug("read_s3_success", path=path)
     return file
