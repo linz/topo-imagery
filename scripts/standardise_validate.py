910655b6d6d1ffae08342117cada9e9ff45655ce
Paul Fouquet <pfouquet@linz.govt.nz>
2022-08-15 15:30:27 +1200

    feat: run standardising following by non_visual_qa

---
+++ scripts/standardise_validate.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from scripts.cli.cli_helper import parse_source
+from scripts.non_visual_qa import non_visual_qa
+from scripts.standardising import standardising
+
+
+def standardise_validate(files: List[str]) -> None:
+    standardising(files)
+
+
+def main() -> None:
+    source = parse_source()
+    standardised_files = standardising(source)
+    non_visual_qa(standardised_files)
+
+
+if __name__ == "__main__":
+    main()
