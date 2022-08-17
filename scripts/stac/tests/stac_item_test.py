e77a9ab204b3b08943a961ad0f46f3a784120cb1
Megan Davidson <mdavidson@linz.govt.nz>
2022-08-17 16:13:38 +1200

    fix: format

--- scripts/stac/tests/stac_item_test.py
+++ scripts/stac/tests/stac_item_test.py
@@ -8,6 +8,11 @@ def test_imagery_stac_item() -> None:
     item.create_stac_item(path, "2021-01-27 00:00:00Z")
     assert item.id == "RGB_BD33_0709"
     assert item.stac["id"] == "RGB_BD33_0709"
-    assert item.stac["geometry"]["coordinates"] == [[[1799667.5, 5815977.0], [1800422.5, 5815977.0], [1800422.5, 5814986.0], [1799667.5, 5814986.0]]]
+    assert item.stac["geometry"]["coordinates"] == [
+        [[1799667.5, 5815977.0], [1800422.5, 5815977.0], [1800422.5, 5814986.0], [1799667.5, 5814986.0]]
+    ]
     assert item.stac["bbox"] == [1799667.5, 5815977.0, 1800422.5, 5814986.0]
-    assert item.stac["assets"]["image"]["checksum:multihash"] == "1220cdef68d62fb912110b810e62edc53de07f7a44fb2b310db700e9d9dd58baa6b4"
+    assert (
+        item.stac["assets"]["image"]["checksum:multihash"]
+        == "1220cdef68d62fb912110b810e62edc53de07f7a44fb2b310db700e9d9dd58baa6b4"
+    )
