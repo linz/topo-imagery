3f4a3bed092acee463cbdfed2efbc64d1c583cff
Alice Fage <afage@linz.govt.nz>
2022-08-23 16:33:38 +1200

    feat: gdal concurrency TDE-457 (#96)
    * feat: parallelise gdal_translate command wip
    
    * fix: detect if running on Argo and set concurrency
    
    * fix: assign output_files
    
    * fix: simplify is_argo
    
    * fix: rename function
    
    * fix: standardise_validate script
    
    * fix: remove print statement
    
    * fix: simplify is_argo
    
    * fix: use with for pool
    
    * refactor: move is_argo to cli helper
    
    * fix: make concurrency setting more intuitive
    
    * fix: simplify concurrency code paths
    
    * fix: standardise validate to use new concurrency structure

--- scripts/standardise_validate.py
+++ scripts/standardise_validate.py
@@ -1,11 +1,14 @@
-from scripts.cli.cli_helper import parse_source
+from scripts.cli.cli_helper import is_argo, parse_source
 from scripts.non_visual_qa import non_visual_qa
-from scripts.standardising import standardising
+from scripts.standardising import start_standardising
 
 
 def main() -> None:
+    concurrency: int = 1
     source = parse_source()
-    standardised_files = standardising(source)
+    if is_argo():
+        concurrency = 4
+    standardised_files = start_standardising(source, concurrency)
     non_visual_qa(standardised_files)
 
 
--- scripts/standardising.py
+++ scripts/standardising.py
@@ -1,80 +1,98 @@
 import os
+from multiprocessing import Pool
 from typing import List
 
 from linz_logger import get_log
 
 from scripts.aws.aws_helper import parse_path
-from scripts.cli.cli_helper import parse_source
+from scripts.cli.cli_helper import is_argo, parse_source
 from scripts.files.files_helper import get_file_name_from_path, is_tiff
 from scripts.gdal.gdal_helper import run_gdal
 from scripts.logging.time_helper import time_in_ms
 
 
-def standardising(files: List[str]) -> List[str]:
+def start_standardising(files: List[str], concurrency: int) -> List[str]:
     start_time = time_in_ms()
-    output_folder = "/tmp/"
+    tiff_files = []
     output_files = []
 
     get_log().info("standardising_start", source=files)
 
     for file in files:
-        if not is_tiff(file):
+        if is_tiff(file):
+            tiff_files.append(file)
+        else:
             get_log().info("standardising_file_not_tiff_skipped", file=file)
-            continue
 
-        _, src_file_path = parse_path(file)
-        standardized_file_name = f"standardized_{get_file_name_from_path(src_file_path)}"
-        tmp_file_path = os.path.join(output_folder, standardized_file_name)
-        command = [
-            "gdal_translate",
-            "-q",
-            "-scale",
-            "0",
-            "255",
-            "0",
-            "254",
-            "-a_srs",
-            "EPSG:2193",
-            "-a_nodata",
-            "255",
-            "-b",
-            "1",
-            "-b",
-            "2",
-            "-b",
-            "3",
-            "-of",
-            "COG",
-            "-co",
-            "compress=lzw",
-            "-co",
-            "num_threads=all_cpus",
-            "-co",
-            "predictor=2",
-            "-co",
-            "overview_compress=webp",
-            "-co",
-            "bigtiff=yes",
-            "-co",
-            "overview_resampling=lanczos",
-            "-co",
-            "blocksize=512",
-            "-co",
-            "overview_quality=90",
-            "-co",
-            "sparse_ok=true",
-        ]
-        run_gdal(command, input_file=file, output_file=tmp_file_path)
-        output_files.append(tmp_file_path)
+    with Pool(concurrency) as p:
+        output_files = p.map(standardising, tiff_files)
+        p.close()
+        p.join()
 
     get_log().info("standardising_end", source=files, duration=time_in_ms() - start_time)
 
     return output_files
 
 
+def standardising(file: str) -> str:
+    output_folder = "/tmp/"
+
+    get_log().info("standardising_start", source=file)
+
+    _, src_file_path = parse_path(file)
+    standardized_file_name = f"standardized_{get_file_name_from_path(src_file_path)}"
+    tmp_file_path = os.path.join(output_folder, standardized_file_name)
+
+    command = [
+        "gdal_translate",
+        "-q",
+        "-scale",
+        "0",
+        "255",
+        "0",
+        "254",
+        "-a_srs",
+        "EPSG:2193",
+        "-a_nodata",
+        "255",
+        "-b",
+        "1",
+        "-b",
+        "2",
+        "-b",
+        "3",
+        "-of",
+        "COG",
+        "-co",
+        "compress=lzw",
+        "-co",
+        "num_threads=all_cpus",
+        "-co",
+        "predictor=2",
+        "-co",
+        "overview_compress=webp",
+        "-co",
+        "bigtiff=yes",
+        "-co",
+        "overview_resampling=lanczos",
+        "-co",
+        "blocksize=512",
+        "-co",
+        "overview_quality=90",
+        "-co",
+        "sparse_ok=true",
+    ]
+    run_gdal(command, input_file=file, output_file=tmp_file_path)
+
+    return tmp_file_path
+
+
 def main() -> None:
+    concurrency: int = 1
     source = parse_source()
-    standardising(source)
+    if is_argo():
+        concurrency = 4
+    start_standardising(source, concurrency)
 
 
 if __name__ == "__main__":
