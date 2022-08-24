25a85c1fbc8794bc410760577af051cc285ef079
paulfouquet <86932794+paulfouquet@users.noreply.github.com>
2022-08-25 09:17:02 +1200

    fix: skip non visual qa if no file has been standardised (#97)
    Co-authored-by: Alice Fage <afage@linz.govt.nz>

--- scripts/standardise_validate.py
+++ scripts/standardise_validate.py
@@ -1,3 +1,5 @@
+from linz_logger import get_log
+
 from scripts.cli.cli_helper import is_argo, parse_source
 from scripts.non_visual_qa import non_visual_qa
 from scripts.standardising import start_standardising
@@ -9,7 +11,12 @@ def main() -> None:
     if is_argo():
         concurrency = 4
     standardised_files = start_standardising(source, concurrency)
-    non_visual_qa(standardised_files)
+    if standardised_files:
+        non_visual_qa(standardised_files)
+    else:
+        get_log().info(
+            "Non Visual QA skipped because no file has been standardised", action="standardise_validate", reason="skip"
+        )
 
 
 if __name__ == "__main__":
