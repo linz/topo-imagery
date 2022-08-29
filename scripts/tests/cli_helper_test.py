bc33ace11a3c81da7c1e333acb7edbd90f93e1af
Megan Davidson <mdavidson@linz.govt.nz>
2022-08-30 09:18:12 +1200

    test: add tests for date formatting and validating functions

--- scripts/tests/cli_helper_test.py
+++ scripts/tests/cli_helper_test.py
@@ -1,4 +1,9 @@
-from scripts.cli.cli_helper import format_source
+import argparse
+from datetime import datetime
+
+import pytest
+
+from scripts.cli.cli_helper import format_date, format_source, valid_date
 
 
 def test_format_source_from_basemaps_cli_file() -> None:
@@ -39,3 +44,30 @@ def test_format_source_json_loading_error() -> None:
     assert isinstance(file_list, list)
     assert len(file_list) == 1
     assert file_list == ["[s3://test/image_one.tiff"]
+
+
+def test_format_date() -> None:
+    """example: --start_datetime 2022-01-22"""
+    s = "2022-01-22"
+    assert format_date(datetime.strptime(s, "%Y-%m-%d")) == "2022-01-21T11:00:1642719600Z"
+
+
+def test_valid_date() -> None:
+    """example:  --start_datetime 2022-01-22"""
+    assert valid_date("2022-01-22") == datetime.strptime("2022-01-22", "%Y-%m-%d")
+
+
+def test_invalid_date() -> None:
+    """example:  --start_datetime 22-01-22"""
+    with pytest.raises(argparse.ArgumentTypeError) as e:
+        valid_date("22-01-22")
+        assert "not a valid date" in str(e.value).lower()
+    with pytest.raises(argparse.ArgumentTypeError) as e:
+        valid_date("2022-22-01")
+        assert "not a valid date" in str(e.value).lower()
+    with pytest.raises(argparse.ArgumentTypeError) as e:
+        valid_date("22-01-2022")
+        assert "not a valid date" in str(e.value).lower()
+    with pytest.raises(argparse.ArgumentTypeError) as e:
+        valid_date("2022/01/22")
+        assert "not a valid date" in str(e.value).lower()
