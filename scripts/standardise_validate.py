e38325137d79e684b523d3e52ab7d3d80a1a20a8
Megan Davidson <mdavidson@linz.govt.nz>
2022-09-09 16:08:53 +1200

    feat: update combined script
    standardise, non visual qa, create items

--- scripts/standardise_validate.py
+++ scripts/standardise_validate.py
@@ -2,18 +2,31 @@ import argparse
 
 from linz_logger import get_log
 
-from scripts.cli.cli_helper import format_source, is_argo
+from scripts.cli.cli_helper import format_date, format_source, is_argo, valid_date
+from scripts.create_stac_items import create_imagery_items
 from scripts.non_visual_qa import non_visual_qa
 from scripts.standardising import start_standardising
 
 
 def main() -> None:
-
     concurrency: int = 1
-    parse_args = argparse.ArgumentParser()
-    parse_args.add_argument("--preset", dest="preset", required=True)
-    parse_args.add_argument("--source", dest="source", nargs="+", required=True)
-    arguments = parse_args.parse_args()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--preset", dest="preset", required=True)
+    parser.add_argument("--source", dest="source", nargs="+", required=True)
+    parser.add_argument(
+        "--start_datetime", dest="start_datetime", help="start datetime in format YYYY-MM-DD", type=valid_date, required=True
+    )
+    parser.add_argument(
+        "--end_datetime", dest="end_datetime", help="end datetime in format YYYY-MM-DD", type=valid_date, required=True
+    )
+    parser.add_argument("--collection", dest="collection", help="path to collection.json", required=True)
+    arguments = parser.parse_args()
+
+    source = format_source(arguments.source)
+    start_datetime = format_date(arguments.start_datetime)
+    end_datetime = format_date(arguments.end_datetime)
+    collection_path = arguments.collection
 
     source = format_source(arguments.source)
 
@@ -23,6 +36,7 @@ def main() -> None:
     standardised_files = start_standardising(source, arguments.preset, concurrency)
     if standardised_files:
         non_visual_qa(standardised_files)
+        create_imagery_items(standardised_files, start_datetime, end_datetime, collection_path)
     else:
         get_log().info(
             "Non Visual QA skipped because no file has been standardised", action="standardise_validate", reason="skip"
