9a6ae29174382e7304644fff682cf88a91315fb4
Megan Davidson <mdavidson@linz.govt.nz>
2022-09-09 15:58:58 +1200

    feat: implement update collection

--- scripts/stac/imagery/collection.py
+++ scripts/stac/imagery/collection.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Any, Dict, List, Optional
 
 import ulid
@@ -27,17 +28,80 @@ class ImageryCollection:
             raise Exception("incorrect initialising parameters must have 'stac' or 'title and description'")
 
     def add_link(self, href: str, rel: str = "item", file_type: str = "application/json") -> None:
-        # Will be implemented in Future PR
-        pass
+        self.stac["links"].append({"rel": rel, "href": href, "type": file_type})
 
     def update_spatial_extent(self, item_bbox: List[float]) -> None:
-        # Will be implemented in Future PR
-        pass
+        if "extent" not in self.stac:
+            self.update_extent(bbox=item_bbox)
+            return
+        if not self.stac["extent"]["spatial"]["bbox"]:
+            self.update_extent(bbox=item_bbox)
+            return
+
+        bbox = self.stac["extent"]["spatial"]["bbox"]
+        min_x = min(bbox[0], bbox[2])
+        max_x = max(bbox[0], bbox[2])
+        min_y = min(bbox[1], bbox[3])
+        max_y = max(bbox[1], bbox[3])
+
+        item_min_x = min(item_bbox[0], item_bbox[2])
+        item_max_x = max(item_bbox[0], item_bbox[2])
+        item_min_y = min(item_bbox[1], item_bbox[3])
+        item_max_y = max(item_bbox[1], item_bbox[3])
+
+        if item_min_x < min_x:
+            min_x = item_min_x
+        if item_min_y < min_y:
+            min_y = item_min_y
+        if item_max_x > max_x:
+            max_x = item_max_x
+        if item_max_y > max_y:
+            max_y = item_max_y
+
+        self.update_extent(bbox=[min_x, min_y, max_x, max_y])
 
     def update_temporal_extent(self, item_start_datetime: str, item_end_datetime: str) -> None:
-        # Will be implemented in Future PR
-        pass
+        if "extent" not in self.stac:
+            self.update_extent(interval=[item_start_datetime, item_end_datetime])
+            return
+        if not self.stac["extent"]["temporal"]["interval"]:
+            self.update_extent(interval=[item_start_datetime, item_end_datetime])
+            return
+
+        interval = self.stac["extent"]["temporal"]["interval"]
+
+        item_start = datetime.strptime(item_start_datetime, "%Y-%m-%dT%H:%M:%SZ")
+        item_end = datetime.strptime(item_end_datetime, "%Y-%m-%dT%H:%M:%SZ")
+
+        collection_datetimes = []
+        for date in interval:
+            collection_datetimes.append(datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ"))
+
+        start_datetime = min(collection_datetimes[0], collection_datetimes[1])
+        end_datetime = max(collection_datetimes[0], collection_datetimes[1])
+
+        if item_start < start_datetime:
+            start_datetime = item_start
+        if item_end > end_datetime:
+            end_datetime = item_end
+
+        self.update_extent(
+            interval=[
+                start_datetime.strftime("%Y-%m-%dT%H:%M:%SZ"),
+                end_datetime.strftime("%Y-%m-%dT%H:%M:%SZ"),
+            ]
+        )
 
     def update_extent(self, bbox: Optional[List[float]] = None, interval: Optional[List[str]] = None) -> None:
-        # Will be implemented in Future PR
-        pass
+        if "extent" not in self.stac:
+            self.stac["extent"] = {
+                "spatial": {
+                    "bbox": bbox,
+                },
+                "temporal": {"interval": interval},
+            }
+            return
+        if bbox:
+            self.stac["extent"]["spatial"]["bbox"] = bbox
+        if interval:
+            self.stac["extent"]["temporal"]["interval"] = interval
--- scripts/stac/tests/collection_test.py
+++ scripts/stac/tests/collection_test.py
@@ -8,3 +8,15 @@ def test_imagery_stac_collection_initialise() -> None:
 
     assert collection.stac["title"] == title
     assert collection.stac["description"] == description
+
+
+def test_imagery_stac_collection_update() -> None:
+    title = "Test Urban Imagery"
+    description = "Test Urban Imagery Description"
+    bbox = [1799667.5, 5815977.0, 1800422.5, 5814986.0]
+    start_datetime = "2021-01-27T00:00:00Z"
+    end_datetime = "2021-01-27T00:00:00Z"
+    collection = ImageryCollection(title, description)
+    collection.update_spatial_extent(bbox)
+    collection.update_temporal_extent(start_datetime, end_datetime)
+    assert collection.stac["extent"]["temporal"]["interval"] == [start_datetime, end_datetime]
