bc924554fca09a7e551dcc29d48eb973d64d88c8
Megan Davidson <mdavidson@linz.govt.nz>
2022-09-21 11:28:59 +1200

    test: add more tests

--- scripts/stac/tests/collection_test.py
+++ scripts/stac/tests/collection_test.py
@@ -20,3 +20,24 @@ def test_imagery_stac_collection_update() -> None:
     collection.update_spatial_extent(bbox)
     collection.update_temporal_extent(start_datetime, end_datetime)
     assert collection.stac["extent"]["temporal"]["interval"] == [start_datetime, end_datetime]
+
+
+def test_imagery_stac_collction_update_twice() -> None:
+    title = "Test Urban Imagery"
+    description = "Test Urban Imagery Description"
+    collection = ImageryCollection(title, description)
+
+    bbox_one = [174.889641, -41.217532, 174.902344, -41.203521]
+    start_datetime_one = "2021-01-27T00:00:00Z"
+    end_datetime_one = "2021-01-27T00:00:00Z"
+    collection.update_spatial_extent(bbox_one)
+    collection.update_temporal_extent(start_datetime_one, end_datetime_one)
+
+    bbox_two = [174.917643, -41.211157, 174.922965, -41.205490]
+    start_datetime_two = "2021-02-01T00:00:00Z"
+    end_datetime_two = "2021-02-20T00:00:00Z"
+    collection.update_spatial_extent(bbox_two)
+    collection.update_temporal_extent(start_datetime_two, end_datetime_two)
+
+    assert collection.stac["extent"]["temporal"]["interval"] == [start_datetime_one, end_datetime_two]
+    assert collection.stac["extent"]["spatial"]["bbox"] == [174.889641, -41.217532, 174.922965, -41.203521]
