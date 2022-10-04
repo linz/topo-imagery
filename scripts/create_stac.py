948d39a74fdbf999ab0a8bbffc99d7e650a378cd
Megan Davidson <mdavidson@linz.govt.nz>
2022-10-05 09:06:52 +1300

    fix: format

--- scripts/create_stac.py
+++ scripts/create_stac.py
@@ -43,7 +43,11 @@ def main() -> None:
             continue
         gdalinfo_result = gdal_info(file)
         item = create_item(
-            file, format_date(arguments.start_datetime), format_date(arguments.end_datetime), arguments.collection_id, gdalinfo_result
+            file,
+            format_date(arguments.start_datetime),
+            format_date(arguments.end_datetime),
+            arguments.collection_id,
+            gdalinfo_result,
         )
         tmp_file_path = os.path.join("/tmp/", f"{item.stac['id']}.json")
         write(tmp_file_path, json.dumps(item.stac).encode("utf-8"))
