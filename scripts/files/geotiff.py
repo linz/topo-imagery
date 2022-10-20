ae5643c3129743bc010c6e82a9d310a46bca5a40
Alice Fage <afage@linz.govt.nz>
2022-10-21 09:13:20 +1300

    fix: coordinates and bbox in WGS84

--- scripts/files/geotiff.py
+++ scripts/files/geotiff.py
@@ -1,14 +1,11 @@
 from typing import Any, Dict, List, Tuple
 
+from shapely.geometry import Polygon
+
 
 def get_extents(gdalinfo_result: Dict[Any, Any]) -> Tuple[List[List[float]], List[float]]:
-    corner_coordinates = gdalinfo_result["cornerCoordinates"]
 
-    upper_left = [corner_coordinates["upperLeft"][0], corner_coordinates["upperLeft"][1]]
-    upper_right = [corner_coordinates["upperRight"][0], corner_coordinates["upperRight"][1]]
-    lower_left = [corner_coordinates["lowerLeft"][0], corner_coordinates["lowerLeft"][1]]
-    lower_right = [corner_coordinates["lowerRight"][0], corner_coordinates["lowerRight"][1]]
+    geometry = gdalinfo_result["wgs84Extent"]["coordinates"][0]
+    bbox = list(Polygon(geometry).bounds)
 
-    geometry = [upper_left, upper_right, lower_right, lower_left]
-    bbox = [upper_left[0], upper_left[1], lower_right[0], lower_right[1]]
     return geometry, bbox
