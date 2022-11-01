60c30d12c353f244b8cbbde85805e742233bd1ef
Megan Davidson <mdavidson@linz.govt.nz>
2022-11-01 14:17:06 +1300

    feat: flatten multiple collections

---
+++ scripts/flatten_collections.py
@@ -0,0 +1,65 @@
+import argparse
+import json
+
+from linz_logger import get_log
+
+from scripts.cli.cli_helper import format_source
+from scripts.files.files_helper import is_json
+from scripts.files.fs import read, write
+from scripts.stac.imagery.collection import ImageryCollection
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--source", dest="source", nargs="+", required=True)
+    parser.add_argument("--collection_id", dest="collection_id", required=True)
+    parser.add_argument("--destination", dest="destination", required=True)
+
+    arguments = parser.parse_args()
+
+    source = format_source(arguments.source)
+
+    collection = None
+
+    for file in source:
+
+        if not is_json(file):
+            get_log().info("skipping file as not json", file=file, action="merge_collection", reason="skip")
+            continue
+
+        if not file.endswith("/collection.json"):
+            get_log().info("skipping file as not collection.json", file=file, action="merge_collection", reason="skip")
+            continue
+
+        partial_stac = json.loads(read(file).decode("utf-8"))
+
+        if not arguments.collection_id == partial_stac["id"]:
+            get_log().info(
+                "skipping file as file collection.id does not match input ulid",
+                file=file,
+                action="merge_collections",
+                reason="skip",
+            )
+            continue
+
+        if not collection:
+            # note: this will need to change when we add more fields, for example we may need `collection_from_json()` (01/11/2022)
+            collection = ImageryCollection(title=partial_stac["title"], description=partial_stac["description"], collection_id=arguments.collection_id)
+            continue
+
+        # merge links
+        for link in partial_stac["links"]:
+            if link["rel"] != "self":
+                collection.add_link(href=link["href"], rel=link["rel"], file_type=link["type"])
+
+        collection.update_spatial_extent(partial_stac["extent"]["spatial"]["bbox"])
+
+        start_datetime = min(partial_stac["extent"]["spatial"]["bbox"][0], partial_stac["extent"]["spatial"]["bbox"][1])
+        end_datetime = max(partial_stac["extent"]["spatial"]["bbox"][0], partial_stac["extent"]["spatial"]["bbox"][1])
+        collection.update_temporal_extent(start_datetime, end_datetime)
+    
+    if collection:
+        write(arguments.destination, json.dumps(collection.stac).encode("utf-8"))
+
+if __name__ == "__main__":
+    main()
--- scripts/stac/imagery/collection.py
+++ scripts/stac/imagery/collection.py
@@ -34,12 +34,12 @@ class ImageryCollection:
     def add_link(self, href: str, rel: str = "item", file_type: str = "application/json") -> None:
         self.stac["links"].append({"rel": rel, "href": href, "type": file_type})
 
-    def update_spatial_extent(self, item_bbox: List[float]) -> None:
+    def update_spatial_extent(self, input_bbox: List[float]) -> None:
         if "extent" not in self.stac:
-            self.update_extent(bbox=item_bbox)
+            self.update_extent(bbox=input_bbox)
             return
         if self.stac["extent"]["spatial"]["bbox"] == [None]:
-            self.update_extent(bbox=item_bbox)
+            self.update_extent(bbox=input_bbox)
             return
 
         bbox = self.stac["extent"]["spatial"]["bbox"][0]
@@ -49,46 +49,45 @@ class ImageryCollection:
         min_y = min(bbox[1], bbox[3])
         max_y = max(bbox[1], bbox[3])
 
-        item_min_x = min(item_bbox[0], item_bbox[2])
-        item_max_x = max(item_bbox[0], item_bbox[2])
-        item_min_y = min(item_bbox[1], item_bbox[3])
-        item_max_y = max(item_bbox[1], item_bbox[3])
+        input_min_x = min(input_bbox[0], input_bbox[2])
+        input_max_x = max(input_bbox[0], input_bbox[2])
+        input_min_y = min(input_bbox[1], input_bbox[3])
+        input_max_y = max(input_bbox[1], input_bbox[3])
 
-        if item_min_x < min_x:
-            min_x = item_min_x
-        if item_min_y < min_y:
-            min_y = item_min_y
-        if item_max_x > max_x:
-            max_x = item_max_x
-        if item_max_y > max_y:
-            max_y = item_max_y
+        if input_min_x < min_x:
+            min_x = input_min_x
+        if input_min_y < min_y:
+            min_y = input_min_y
+        if input_max_x > max_x:
+            max_x = input_max_x
+        if input_max_y > max_y:
+            max_y = input_max_y
 
         self.update_extent(bbox=[min_x, min_y, max_x, max_y])
 
-    def update_temporal_extent(self, item_start_datetime: str, item_end_datetime: str) -> None:
+    def update_temporal_extent(self, input_start_datetime: str, input_end_datetime: str) -> None:
         if "extent" not in self.stac:
-            self.update_extent(interval=[item_start_datetime, item_end_datetime])
+            self.update_extent(interval=[input_start_datetime, input_end_datetime])
             return
         if self.stac["extent"]["temporal"]["interval"] == [None]:
-            self.update_extent(interval=[item_start_datetime, item_end_datetime])
+            self.update_extent(interval=[input_start_datetime, input_end_datetime])
             return
 
-        interval = self.stac["extent"]["temporal"]["interval"][0]
-
-        item_start = datetime.strptime(item_start_datetime, "%Y-%m-%dT%H:%M:%SZ")
-        item_end = datetime.strptime(item_end_datetime, "%Y-%m-%dT%H:%M:%SZ")
+        input_start = datetime.strptime(input_start_datetime, "%Y-%m-%dT%H:%M:%SZ")
+        input_end = datetime.strptime(input_end_datetime, "%Y-%m-%dT%H:%M:%SZ")
 
         collection_datetimes = []
+        interval = self.stac["extent"]["temporal"]["interval"][0]
         for date in interval:
             collection_datetimes.append(datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ"))
 
         start_datetime = min(collection_datetimes[0], collection_datetimes[1])
         end_datetime = max(collection_datetimes[0], collection_datetimes[1])
 
-        if item_start < start_datetime:
-            start_datetime = item_start
-        if item_end > end_datetime:
-            end_datetime = item_end
+        if input_start < start_datetime:
+            start_datetime = input_start
+        if input_end > end_datetime:
+            end_datetime = input_end
 
         self.update_extent(
             interval=[
