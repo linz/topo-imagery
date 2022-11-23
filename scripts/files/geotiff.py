3eb4a103652ceda1ed8492d171203743d4e8f5f3
Alice Fage <afage@linz.govt.nz>
2022-11-23 14:18:33 +1300

    fix: tidy up geometry and bbox

--- scripts/files/geotiff.py
+++ scripts/files/geotiff.py
@@ -5,7 +5,7 @@ from shapely.geometry import Polygon
 
 def get_extents(gdalinfo_result: Dict[Any, Any]) -> Tuple[List[List[float]], List[float]]:
 
-    geometry = gdalinfo_result["wgs84Extent"]["coordinates"][0]
-    bbox = list(Polygon(geometry).bounds)
+    geometry = gdalinfo_result["wgs84Extent"]
+    bbox = Polygon(geometry["coordinates"][0]).bounds
 
     return geometry, bbox
