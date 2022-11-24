02b06b51a58650ffbe1a18a4bedd4996251c6c16
Alice Fage <afage@linz.govt.nz>
2022-11-25 09:09:32 +1300

    fix: tidy up geometry and bbox TDE-522 (#227)
    * fix: tidy up geometry and bbox
    
    * fix: test formatting

--- scripts/stac/tests/item_test.py
+++ scripts/stac/tests/item_test.py
@@ -5,7 +5,10 @@ from scripts.stac.imagery.item import ImageryItem
 
 def test_imagery_stac_item(mocker) -> None:  # type: ignore
     # mock functions that interact with files
-    geometry = [[1799667.5, 5815977.0], [1800422.5, 5815977.0], [1800422.5, 5814986.0], [1799667.5, 5814986.0]]
+    geometry = {
+        "type": "Polygon",
+        "coordinates": [[[1799667.5, 5815977.0], [1800422.5, 5815977.0], [1800422.5, 5814986.0], [1799667.5, 5814986.0]]],
+    }
     bbox = [1799667.5, 5815977.0, 1800422.5, 5814986.0]
     checksum = "1220cdef68d62fb912110b810e62edc53de07f7a44fb2b310db700e9d9dd58baa6b4"
     mocker.patch("scripts.stac.util.checksum.multihash_as_hex", return_value=checksum)
@@ -23,7 +26,8 @@ def test_imagery_stac_item(mocker) -> None:  # type: ignore
     assert item.stac["properties"]["start_datetime"] == start_datetime
     assert item.stac["properties"]["end_datetime"] == end_datetime
     assert item.stac["properties"]["datetime"] is None
-    assert item.stac["geometry"]["coordinates"] == [geometry]
+    assert item.stac["geometry"]["coordinates"] == geometry["coordinates"]
+    assert item.stac["geometry"] == geometry
     assert item.stac["bbox"] == bbox
     assert item.stac["assets"]["visual"]["file:checksum"] == checksum
     assert {"rel": "self", "href": f"./{id_}.json", "type": "application/json"} in item.stac["links"]
