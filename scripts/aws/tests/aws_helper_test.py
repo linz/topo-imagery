19cb90b65f497c0f6e8edafe878414ec1cc99cd4
paul <pfouquet@linz.govt.nz>
2023-03-08 15:38:06 +1300

    fix: trailing char in local path is removed

--- scripts/aws/tests/aws_helper_test.py
+++ scripts/aws/tests/aws_helper_test.py
@@ -2,12 +2,19 @@ from scripts.aws.aws_helper import parse_path
 from scripts.cli.cli_helper import is_argo
 
 
-def test_parse_path() -> None:
+def test_parse_path_s3() -> None:
     s3_path = "s3://bucket-name/path/to/the/file.test"
     path = parse_path(s3_path)
 
     assert path.bucket == "bucket-name"
     assert path.key == "path/to/the/file.test"
+
+def test_parse_path_local() -> None:
+    local_path = "/home/tmp/file.test"
+    _, file_path = parse_path(local_path)
+
+    #assert path.bucket == "bucket-name"
+    assert file_path == "/home/tmp/file.test"
 
 
 def test_is_argo() -> None:
