6c365634f4c20cfc773688ee1dc68646ae201c46
Paul Fouquet <pfouquet@linz.govt.nz>
2023-08-21 15:29:12 +1200

    refactor: allows to re-use thumbnails()

--- scripts/thumbnails.py
+++ scripts/thumbnails.py
@@ -2,10 +2,11 @@ import argparse
 import os
 import sys
 import tempfile
+from typing import List
 
 from linz_logger import get_log
 
-from scripts.cli.cli_helper import InputParameterError, load_input_files
+from scripts.cli.cli_helper import InputParameterError, TileFiles, load_input_files
 from scripts.files.files_helper import get_file_name_from_path, is_GTiff, is_tiff
 from scripts.files.fs import exists, read, write
 from scripts.gdal.gdal_helper import run_gdal
@@ -13,26 +14,12 @@ from scripts.gdal.gdal_preset import get_thumbnail_command
 from scripts.logging.time_helper import time_in_ms
 
 
-def main() -> None:
+def thumbnails(tile_files: List[TileFiles], target: str) -> int:
     start_time = time_in_ms()
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--from-file", dest="from_file", required=True, help="The path to a json file containing the input tiffs"
-    )
-    parser.add_argument("--target", dest="target", required=True, help="Output location path")
-    arguments = parser.parse_args()
-    from_file = arguments.from_file
+    get_log().info("thumbnails_start")
     count = 0
 
-    get_log().info("thumbnails_start", input=from_file)
-
     with tempfile.TemporaryDirectory() as tmp_path:
-        try:
-            tile_files = load_input_files(from_file)
-        except InputParameterError as e:
-            get_log().error("thumbnails_input_error", error=str(e))
-            sys.exit(1)
-
         for tile in tile_files:
             # Verify 1 input for 1 output
             if len(tile.inputs) > 1:
@@ -45,7 +32,7 @@ def main() -> None:
                 continue
 
             basename = get_file_name_from_path(file)
-            target_thumbnail = os.path.join(arguments.target, f"{basename}-thumbnail.jpg")
+            target_thumbnail = os.path.join(target, f"{basename}-thumbnail.jpg")
             # Verify the thumbnail has not been already generated
             if exists(target_thumbnail):
                 get_log().info("thumbnails_already_exists", path=target_thumbnail)
@@ -74,7 +61,26 @@ def main() -> None:
             write(target_thumbnail, read(tmp_thumbnail))
             count += 1
 
-        get_log().info("thumbnails_end", count=count, duration=time_in_ms() - start_time)
+    get_log().info("thumbnails_end", count=count, duration=time_in_ms() - start_time)
+    return count
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--from-file", dest="from_file", required=True, help="The path to a json file containing the input tiffs"
+    )
+    parser.add_argument("--target", dest="target", required=True, help="Output location path")
+    arguments = parser.parse_args()
+    from_file = arguments.from_file
+
+    try:
+        tile_files = load_input_files(from_file)
+    except InputParameterError as e:
+        get_log().error("thumbnails_input_error", error=str(e))
+        sys.exit(1)
+
+    thumbnails(tile_files, arguments.target)
 
 
 if __name__ == "__main__":
