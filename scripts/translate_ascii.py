61a8a14e62c5fb7230272ac5a9543dfb5da5eb73
Megan Davidson <mdavidson@linz.govt.nz>
2023-09-25 10:22:43 +1300

    fix: use concurrency

--- scripts/translate_ascii.py
+++ scripts/translate_ascii.py
@@ -3,9 +3,13 @@ import json
 import os
 import tempfile
 
+from scripts.cli.cli_helper import is_argo
 from scripts.files.fs import find_sidecars, read, write_all
 from scripts.gdal.gdal_helper import run_gdal
 from scripts.gdal.gdal_preset import get_ascii_translate_command
+
+from functools import partial
+from multiprocessing import Pool
 
 
 def main() -> None:
@@ -25,24 +29,39 @@ def main() -> None:
     parser.add_argument("--target", dest="target", required=True, help="Output location path")
     arguments = parser.parse_args()
 
-    asc_files = json.loads(read(arguments.from_file))
+    asc_files = json.loads(read(arguments.from_file))[0]
+
+    concurrency: int = 1
+    if is_argo():
+        concurrency = 4
 
     with tempfile.TemporaryDirectory() as tmp_path:
-        tiffs = []
-        for ls in asc_files:
-            for asc in ls:
-                # translate from ascii to geotiff using GDAL
-                filename = os.path.splitext(os.path.basename(asc))[0]
-                tiff = os.path.join(tmp_path, f"{filename}.tiff")
-                run_gdal(get_ascii_translate_command(), input_file=asc, output_file=tiff)
-                tiffs.append(tiff)
-        write_all(inputs=tiffs, target=arguments.target)
+        with Pool(concurrency) as p:
+            ascii_tiffs = p.map(
+                partial(
+                    translate_ascii,
+                    tmp_path=tmp_path,
+                ),
+                asc_files,
+            )
+        p.close()
+        p.join()
+
+        write_all(inputs=ascii_tiffs, target=arguments.target)
 
         # copy any sidecar files to target
         sidecars = []
         for ls in asc_files:
             sidecars += find_sidecars(ls, [".prj", ".tfw"])
         write_all(inputs=sidecars, target=arguments.target)
+
+
+def translate_ascii(file: str, tmp_path: str) -> str:
+    # translate from ascii to geotiff using GDAL
+    filename = os.path.splitext(os.path.basename(file))[0]
+    tiff = os.path.join(tmp_path, f"{filename}.tiff")
+    run_gdal(get_ascii_translate_command(), input_file=file, output_file=tiff)
+    return tiff
 
 
 if __name__ == "__main__":
