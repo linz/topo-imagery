dcd4b68c4041ab143e7de80fa78b88347329edc6
Megan Davidson <mdavidson@linz.govt.nz>
2023-12-01 09:33:08 +1300

    test: autogenerate title & description tests

---
+++ scripts/stac/tests/generate_description_test.py
@@ -0,0 +1,44 @@
+from scripts.stac.imagery.generate_metadata import generate_description
+from datetime import datetime
+
+
+def test_generate_description_imagery() -> None:
+    description = "Orthophotography within the Auckland region captured in the 2023 flying season."
+    generated_description = generate_description("Rural Aerial Photos", "Auckland", datetime(2023, 1, 1), datetime(2023, 2, 2))
+    assert generated_description == description
+
+
+def test_generate_description_elevation() -> None:
+    description = "Digital Elevation Model within the Auckland region in 2023."
+    generated_description = generate_description("DEM", "Auckland", datetime(2023, 1, 1), datetime(2023, 2, 2))
+    assert generated_description == description
+
+
+def test_generate_description_elevation_location_input() -> None:
+    description = "Digital Elevation Model within the Auckland - Central region in 2023."
+    generated_description = generate_description(
+        "DEM", "Auckland", datetime(2023, 1, 1), datetime(2023, 2, 2), location="Central"
+    )
+    assert generated_description == description
+
+
+def test_generate_description_satellite_imagery() -> None:
+    description = "Satellite imagery within the Auckland region captured in 2023."
+    generated_description = generate_description("Satellite Imagery", "Auckland", datetime(2023, 1, 1), datetime(2023, 2, 2))
+    assert generated_description == description
+
+
+def test_generate_description_historic_imagery() -> None:
+    description = "Scanned aerial imagery within the Auckland region captured in 2000."
+    generated_description = generate_description(
+        "Aerial Photos", "Auckland", datetime(2000, 1, 1), datetime(2000, 2, 2), historic_survey_number="SNC8844"
+    )
+    assert generated_description == description
+
+
+def test_generate_description_event() -> None:
+    description = "Orthophotography within the Hawke's Bay region captured in the 2023 flying season, published as a record of the Cyclone Gabrielle event."
+    generated_description = generate_description(
+        "Rural Aerial Photos", "Hawke's Bay", datetime(2023, 1, 1), datetime(2023, 2, 2), event="Cyclone Gabrielle"
+    )
+    assert generated_description == description
---
+++ scripts/stac/tests/generate_title_test.py
@@ -0,0 +1,78 @@
+from scripts.stac.imagery.generate_metadata import generate_title, generate_description
+from datetime import datetime
+
+
+def test_generate_imagery_title() -> None:
+    title = "Auckland 0.3m Rural Aerial Photos (2023)"
+    generated_title = generate_title("Rural Aerial Photos", "Auckland", "0.3m", datetime(2023, 1, 1), datetime(2023, 2, 2))
+    assert generated_title == title
+
+
+def test_generate_dem_title() -> None:
+    title = "Auckland LiDAR 0.3m DEM (2023)"
+    generated_title = generate_title("DEM", "Auckland", "0.3m", datetime(2023, 1, 1), datetime(2023, 2, 2))
+    assert generated_title == title
+
+
+def test_generate_dsm_title() -> None:
+    title = "Auckland LiDAR 0.3m DSM (2023)"
+    generated_title = generate_title("DSM", "Auckland", "0.3m", datetime(2023, 1, 1), datetime(2023, 2, 2))
+    assert generated_title == title
+
+
+def test_generate_satellite_imagery_title() -> None:
+    title = "Auckland 0.5m Satellite Imagery (2023)"
+    generated_title = generate_title("Satellite Imagery", "Auckland", "0.5m", datetime(2023, 1, 1), datetime(2023, 2, 2))
+    assert generated_title == title
+
+
+def test_generate_historic_imagery_title() -> None:
+    title = "Auckland 0.3m SNC8844 (2000)"
+    generated_title = generate_title(
+        "Aerial Photos", "Auckland", "0.3m", datetime(2000, 1, 1), datetime(2000, 2, 2), historic_survey_number="SNC8844"
+    )
+    assert generated_title == title
+
+
+def test_generate_title_long_date() -> None:
+    title = "Auckland 0.3m Urban Aerial Photos (2023 - 2024)"
+    generated_title = generate_title("Urban Aerial Photos", "Auckland", "0.3m", datetime(2023, 1, 1), datetime(2024, 2, 2))
+    assert generated_title == title
+
+
+def test_generate_title_location() -> None:
+    title = "Banks Penninsula 0.3m Rural Aerial Photos (2023)"
+    generated_title = generate_title(
+        "Rural Aerial Photos", "Canterbury", "0.3m", datetime(2023, 1, 1), datetime(2023, 2, 2), location="Banks Penninsula"
+    )
+    assert generated_title == title
+
+
+def test_generate_title_event_imagery() -> None:
+    title = "Hawke's Bay 0.3m Cyclone Gabrielle Rural Aerial Photos (2023)"
+    generated_title = generate_title(
+        "Rural Aerial Photos", "Hawke's Bay", "0.3m", datetime(2023, 1, 1), datetime(2023, 2, 2), event="Cyclone Gabrielle"
+    )
+    assert generated_title == title
+
+
+def test_generate_title_event_elevation() -> None:
+    title = "Hawke's Bay - Cyclone Gabrielle LiDAR 0.3m DSM (2023)"
+    generated_title = generate_title(
+        "DSM", "Hawke's Bay", "0.3m", datetime(2023, 1, 1), datetime(2023, 2, 2), event="Cyclone Gabrielle"
+    )
+    assert generated_title == title
+
+
+def test_generate_title_event_satellite_imagery() -> None:
+    title = "Hawke's Bay 0.5m Cyclone Gabrielle Satellite Imagery (2023)"
+    generated_title = generate_title(
+        "Satellite Imagery", "Hawke's Bay", "0.5m", datetime(2023, 1, 1), datetime(2023, 2, 2), event="Cyclone Gabrielle"
+    )
+    assert generated_title == title
+
+
+def test_generate_dsm_title_preview() -> None:
+    title = "Auckland LiDAR 0.3m DSM (2023) - preview"
+    generated_title = generate_title("DSM", "Auckland", "0.3m", datetime(2023, 1, 1), datetime(2023, 2, 2), preview=True)
+    assert generated_title == title
