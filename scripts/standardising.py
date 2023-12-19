2aadc6ee78b546b73800b3615ffd4ea71e8af028
Victor Engmark <vengmark@linz.govt.nz>
2023-12-19 13:30:44 +1300

    refactor: Check tile byte counts only in first page

--- scripts/standardising.py
+++ scripts/standardising.py
@@ -2,10 +2,10 @@ import os
 import tempfile
 from functools import partial
 from multiprocessing import Pool
-from typing import Iterator, List, Optional
+from typing import List, Optional
 
 from linz_logger import get_log
-from tifffile import TiffFile, TiffTags
+from tifffile import TiffFile
 
 from scripts.aws.aws_helper import is_s3
 from scripts.cli.cli_helper import TileFiles
@@ -194,18 +194,10 @@ def standardising(
         # Need GDAL to write to temporary location so no broken files end up in the done folder.
         run_gdal(command, input_file=input_file, output_file=standardized_working_path)
 
-        for tag_set in tag_sets(standardized_working_path):
-            if any(tile_byte_count != 0 for tile_byte_count in tag_set["TileByteCounts"].value):
+        with TiffFile(standardized_working_path) as file_handle:
+            if any(tile_byte_count != 0 for tile_byte_count in file_handle.pages.first.tags["TileByteCounts"].value):
                 write(standardized_file_path, read(standardized_working_path), content_type=ContentType.GEOTIFF.value)
                 return tiff
 
         get_log().info("Skipping empty output image", path=input_file, sourceEPSG=source_epsg, targetEPSG=target_epsg)
         return None
-
-
-def tag_sets(path: str) -> Iterator[TiffTags]:
-    with TiffFile(path) as file_handle:
-        # For performance reasons, inspect the last (usually smallest) pages first
-        for page in reversed(file_handle.pages):
-            if hasattr(page, "tags"):
-                yield page.tags
