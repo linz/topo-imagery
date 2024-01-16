ef7ddf4ba898e64a0d32091d2f1542dd8e03a9ca
Paul Fouquet <pfouquet@linz.govt.nz>
2024-01-17 09:34:27 +1300

    test: add a test for list_files_in_uri

--- scripts/files/tests/fs_s3_test.py
+++ scripts/files/tests/fs_s3_test.py
@@ -7,7 +7,7 @@ from moto.s3.responses import DEFAULT_REGION_NAME
 from pytest import CaptureFixture, raises
 
 from scripts.files.files_helper import ContentType
-from scripts.files.fs_s3 import exists, read, write
+from scripts.files.fs_s3 import exists, list_files_in_uri, read, write
 
 
 @mock_s3  # type: ignore
@@ -124,3 +124,20 @@ def test_exists_object_starting_with_not_exists() -> None:
     file_exists = exists("s3://testbucket/hello/another.fi")
 
     assert file_exists is False
+
+
+@mock_s3  # type: ignore
+def test_list_files_in_uri() -> None:
+    s3 = resource("s3", region_name=DEFAULT_REGION_NAME)
+    boto3_client = client("s3", region_name=DEFAULT_REGION_NAME)
+    s3.create_bucket(Bucket="testbucket")
+    boto3_client.put_object(Bucket="testbucket", Key="data/collection.json", Body=b"test content")
+    boto3_client.put_object(Bucket="testbucket", Key="data/image.tiff", Body=b"test content")
+    boto3_client.put_object(Bucket="testbucket", Key="data/image_meta.xml", Body=b"test content")
+
+    files = list_files_in_uri("s3://testbucket/data/", [".json", "_meta.xml"], boto3_client)
+
+    assert len(files) == 2
+    assert "data/collection.json" in files
+    assert "data/image_meta.xml" in files
+    assert "data/image.tiff" not in files
