3f8e815bc83f0f725ea41972aefa92c4fb3d574b
Alice Fage <afage@linz.govt.nz>
2024-01-17 14:53:57 +1300

    test: for fs functions

---
+++ scripts/files/tests/fs_test.py
@@ -0,0 +1,61 @@
+import json
+
+from boto3 import resource
+from moto import mock_s3
+from moto.s3.responses import DEFAULT_REGION_NAME
+from pytest import CaptureFixture, raises
+
+from scripts.files.fs import read, write_all, write_sidecars, NoSuchFileError
+
+
+def test_read_key_not_found_local() -> None:
+
+    with raises(NoSuchFileError):
+        read("test_dir/test.file")
+
+
+@mock_s3  # type: ignore
+def test_read_key_not_found_s3(capsys: CaptureFixture[str]) -> None:
+    s3 = resource("s3", region_name=DEFAULT_REGION_NAME)
+    s3.create_bucket(Bucket="testbucket")
+
+    with raises(NoSuchFileError):
+        read("s3://testbucket/test.file")
+
+    logs = json.loads(capsys.readouterr().out)
+    assert logs["msg"] == "s3_key_not_found"
+
+
+
+def test_write_all_key_not_found_local() -> None:
+
+    with raises(NoSuchFileError):
+        write_all("s3://testbucket/test.prj", "/tmp")
+
+
+def test_write_sidecars_key_not_found_local(capsys: CaptureFixture[str]) -> None:
+
+    write_sidecars("s3://testbucket/test.prj", "/tmp")
+
+    logs = json.loads(capsys.readouterr().out)
+    assert logs["msg"] == "No sidecar file found; skipping"
+
+
+@mock_s3  # type: ignore
+def test_write_all_key_not_found_s3() -> None:
+    s3 = resource("s3", region_name=DEFAULT_REGION_NAME)
+    s3.create_bucket(Bucket="testbucket")
+
+    with raises(NoSuchFileError):
+        write_all("s3://testbucket/test.tif", "/tmp")
+
+
+@mock_s3  # type: ignore
+def test_write_sidecars_key_not_found_s3(capsys: CaptureFixture[str]) -> None:
+    s3 = resource("s3", region_name=DEFAULT_REGION_NAME)
+    s3.create_bucket(Bucket="testbucket")
+
+    write_sidecars("s3://testbucket/test.prj", "/tmp")
+
+    logs = json.loads(capsys.readouterr().out)
+    assert logs["msg"] == "No sidecar file found; skipping"
