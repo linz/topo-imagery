6ca8cfb26076549c2f63d82fb322d6df608adb94
Paul Fouquet <pfouquet@linz.govt.nz>
2024-02-02 18:32:38 +1300

    refactor: simplify code in tests

--- scripts/files/tests/fs_s3_test.py
+++ scripts/files/tests/fs_s3_test.py
@@ -128,16 +128,16 @@ def test_exists_object_starting_with_not_exists() -> None:
 
 @mock_s3  # type: ignore
 def test_list_files_in_uri() -> None:
+    bucket_name = "testbucket"
     s3 = resource("s3", region_name=DEFAULT_REGION_NAME)
     boto3_client = client("s3", region_name=DEFAULT_REGION_NAME)
-    s3.create_bucket(Bucket="testbucket")
-    boto3_client.put_object(Bucket="testbucket", Key="data/collection.json", Body=b"test content")
-    boto3_client.put_object(Bucket="testbucket", Key="data/image.tiff", Body=b"test content")
-    boto3_client.put_object(Bucket="testbucket", Key="data/image_meta.xml", Body=b"test content")
+    s3.create_bucket(Bucket=bucket_name)
+    boto3_client.put_object(Bucket=bucket_name, Key="data/collection.json", Body=b"")
+    boto3_client.put_object(Bucket=bucket_name, Key="data/image.tiff", Body=b"")
+    boto3_client.put_object(Bucket=bucket_name, Key="data/image_meta.xml", Body=b"")
 
     files = list_files_in_uri("s3://testbucket/data/", [".json", "_meta.xml"], boto3_client)
 
     assert len(files) == 2
-    assert "data/collection.json" in files
-    assert "data/image_meta.xml" in files
+    assert set(files) == {"data/collection.json", "data/image_meta.xml"}
     assert "data/image.tiff" not in files
