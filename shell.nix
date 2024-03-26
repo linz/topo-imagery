7e800b4d7b6edeae37d345cbc6f066f58809f2d1
Victor Engmark <victor@engmark.name>
2024-03-26 17:56:38 +1300

    build: Bump nixpkgs to use latest GDAL

--- shell.nix
+++ shell.nix
@@ -1,19 +1,10 @@
 let
-  nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/nixos/nixpkgs/archive/b06025f1533a1e07b6db3e75151caa155d1c7eb3.tar.gz";
-    sha256 = "1b8dim6xpcg3wyb0xa0w4h4m22npbzl2np822x4r7wiw5wnnzg5a";
-  };
-  pkgs = import nixpkgs {};
-  patchedPkgs = import (pkgs.applyPatches {
-    name = "libtiff: Add LERC support";
-    src = nixpkgs;
-    patches = [
-      (pkgs.fetchpatch {
-        url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/290556.patch";
-        hash = "sha256-J92poRJkI+pE/Z/Eohq0Q61nRH2cya4tTqL8N9XHhJw";
-      })
-    ];
-  }) {};
+  pkgs = import (
+    builtins.fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/7541ec60b6f2d38b76e057135bb5942b78d3370c.tar.gz";
+      sha256 = "1ndqfddqmfzd8lfq439kwbpm70qdblkdsw22qd16ibns8kq215cz";
+    }
+  ) {};
   poetry2nix =
     import (
       builtins.fetchTarball {
@@ -47,7 +38,7 @@ in
       pkgs.bashInteractive
       pkgs.cacert
       pkgs.exiftool
-      patchedPkgs.gdal
+      pkgs.gdal
       pkgs.gitFull
       pkgs.poetry
       pkgs.qgis
