11b59e9bcd783f02886ab04adbc86e5adce3cffb
Paul Fouquet <pfouquet@linz.govt.nz>
2024-09-03 15:53:54 +1200

    test: fix gdal_info

--- scripts/stac/imagery/tests/create_stac_test.py
+++ scripts/stac/imagery/tests/create_stac_test.py
@@ -1,14 +1,19 @@
 from pathlib import Path
+from typing import cast
 
+from scripts.gdal.gdalinfo import GdalInfo
 from scripts.stac.imagery.create_stac import create_item
 
 
 def test_create_item_with_derived_from(tmp_path: Path) -> None:
     derived_from_path = tmp_path / "derived_from_item.json"
     derived_from_path.write_text('{"type": "Feature", "id": "fake_item"}')
+    fake_gdal_info: GdalInfo = cast(
+        GdalInfo, {"wgs84Extent": {"type": "Polygon", "coordinates": [[[0, 1], [1, 1], [1, 0], [0, 0]]]}}
+    )
 
     item = create_item(
-        "./scripts/tests/data/empty.tiff", "2024-01-01", "2024-01-02", "abc123", derived_from=[f"{derived_from_path}"]
+        "./scripts/tests/data/empty.tiff", "2024-01-01", "2024-01-02", "abc123", fake_gdal_info, [f"{derived_from_path}"]
     )
 
     assert {
