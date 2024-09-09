91ac9bc1be1929cb945253e99c67ce08617894a8
Paul Fouquet <pfouquet@linz.govt.nz>
2024-09-10 08:53:18 +1200

    test: derived_from datetimes

--- scripts/stac/imagery/tests/create_stac_test.py
+++ scripts/stac/imagery/tests/create_stac_test.py
@@ -28,3 +28,35 @@ def test_create_item_with_derived_from(tmp_path: Path) -> None:
         "type": "application/json",
         "file:checksum": "12209c3d50f21fdd739de5c76b3c7ca60ee7f5cf69c2cf92b1d0136308cf63d9c5d5",
     } in item.stac["links"]
+
+
+def test_create_item_with_derived_from_datetimes(tmp_path: Path) -> None:
+    derived_from_path_a = tmp_path / "derived_from_item_a.json"
+    derived_from_path_b = tmp_path / "derived_from_item_b.json"
+    fake_item_a = {
+        "type": "Feature",
+        "id": "fake_item",
+        "properties": {"start_datetime": "2024-09-02T12:00:00Z", "end_datetime": "2024-09-02T12:00:00Z"},
+    }
+    fake_item_b = {
+        "type": "Feature",
+        "id": "fake_item",
+        "properties": {"start_datetime": "1998-02-12T11:00:00Z", "end_datetime": "1999-09-02T12:00:00Z"},
+    }
+    derived_from_path_a.write_text(json.dumps(fake_item_a))
+    derived_from_path_b.write_text(json.dumps(fake_item_b))
+    fake_gdal_info: GdalInfo = cast(
+        GdalInfo, {"wgs84Extent": {"type": "Polygon", "coordinates": [[[0, 1], [1, 1], [1, 0], [0, 0]]]}}
+    )
+
+    item = create_item(
+        "./scripts/tests/data/empty.tiff",
+        "2024-01-01",
+        "2024-01-02",
+        "abc123",
+        fake_gdal_info,
+        [derived_from_path_a.as_posix(), derived_from_path_b.as_posix()],
+    )
+
+    assert item.stac["properties"]["start_datetime"] == "1998-02-12T11:00:00Z"
+    assert item.stac["properties"]["end_datetime"] == "2024-09-02T12:00:00Z"
