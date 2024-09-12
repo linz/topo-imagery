0c22357ffc0c83cd3636d276c10cbf9ffa30608b
Paul Fouquet <pfouquet@linz.govt.nz>
2024-09-12 14:42:37 +1200

    Revert "feat: valid_date allows empty"
    This reverts commit f1200346eb1e833e585fbf06fda3879f72649baf.

--- scripts/cli/cli_helper.py
+++ scripts/cli/cli_helper.py
@@ -78,9 +78,7 @@ def is_argo() -> bool:
     return bool(environ.get("ARGO_TEMPLATE"))
 
 
-def valid_date(s: str) -> datetime | None:
-    if not s:
-        return None
+def valid_date(s: str) -> datetime:
     try:
         return parse_rfc_3339_date(s)
     except ValueError as e:
