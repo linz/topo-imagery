d7227ccbfeb9b1226538767f1fb833866d4eb033
Paul Fouquet <pfouquet@linz.govt.nz>
2024-10-25 15:57:01 +1300

    refactor: use environment variable to get GDAL version

--- scripts/standardise_validate.py
+++ scripts/standardise_validate.py
@@ -9,7 +9,7 @@ from scripts.datetimes import format_rfc_3339_nz_midnight_datetime_string
 from scripts.files.file_tiff import FileTiff
 from scripts.files.files_helper import SUFFIX_JSON, ContentType
 from scripts.files.fs import exists, write
-from scripts.gdal.gdal_helper import get_gdal_version, get_srs, get_vfs_path
+from scripts.gdal.gdal_helper import get_srs, get_vfs_path
 from scripts.json_codec import dict_to_json_bytes
 from scripts.stac.imagery.create_stac import create_item
 from scripts.standardising import run_standardising
@@ -107,7 +107,7 @@ def main() -> None:
     if is_argo():
         concurrency = 4
 
-    gdal_version = get_gdal_version()
+    gdal_version = os.environ["GDAL_VERSION"]
 
     tiff_files = run_standardising(
         tile_files,
