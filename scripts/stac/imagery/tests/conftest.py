1d4b532ffb6e281fba11b4f93fbf99b3aad299bc
Tobias Schmidt <13055656+schmidtnz@users.noreply.github.com>
2024-11-05 15:21:27 +1300

    refactor: randomize fake linz slug and deduplicate code

--- scripts/stac/imagery/tests/conftest.py
+++ scripts/stac/imagery/tests/conftest.py
@@ -21,8 +21,3 @@ def fake_collection_metadata() -> Iterator[CollectionMetadata]:
         "geographic_description": None,
     }
     yield collection_metadata
-
-
-@pytest.fixture
-def fake_linz_slug() -> str:
-    return "somewhere-in-new-zealand_2021-2023_0.75m"
