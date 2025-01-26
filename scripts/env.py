127a9f6c13fbd75533b228fcf713c75c4178d922
Paul Fouquet <pfouquet@linz.govt.nz>
2025-01-27 09:25:03 +1300

    test: gdal_info

--- scripts/env.py
+++ scripts/env.py
@@ -1,13 +1,15 @@
 import os
-import subprocess
+
+from scripts.gdal.gdal_helper import gdal_info
 
 
 def main() -> None:
     print(os.environ)
-    gdal_info = subprocess.run(
-        ["gdalinfo", "/vsis3/nz-elevation/new-zealand/new-zealand/dem_1m/2193/BM35.tiff"], capture_output=True, check=True
-    )
-    print(gdal_info)
+    # gdal_info = subprocess.run(
+    #     ["gdalinfo", "/vsis3/nz-elevation/new-zealand/new-zealand/dem_1m/2193/BM35.tiff"], capture_output=True, check=True
+    # )
+    gdalinfo = gdal_info("/vsis3/nz-elevation/new-zealand/new-zealand/dem_1m/2193/BM35.tiff")
+    print(gdalinfo)
 
 
 if __name__ == "__main__":
