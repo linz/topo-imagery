dd7dff6f57c5e85071597c79d37f4d29fcfc6911
Tobias Schmidt <13055656+schmidtnz@users.noreply.github.com>
2025-01-31 11:35:40 +1300

    feat: add '-resolution highest' argument to gdalbuildvrt

--- scripts/gdal/gdal_commands.py
+++ scripts/gdal/gdal_commands.py
@@ -84,7 +84,7 @@ def get_build_vrt_command(files: list[str], output: str = "output.vrt", add_alph
     """
     # `-allow_projection_difference` is passed to workaround different coordinate system descriptions within the same EPSG
     # Having the same EPSG code for all images is already checked by `linz/argo-tasks` `tile-index-validate`
-    gdal_command = ["gdalbuildvrt", "-strict", "-allow_projection_difference"]
+    gdal_command = ["gdalbuildvrt", "-resolution", "highest", "-strict", "-allow_projection_difference"]
     if add_alpha:
         gdal_command.append("-addalpha")
     gdal_command.append(output)
