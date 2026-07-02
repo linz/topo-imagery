e86454d33da8252b76a94fcb284689bef687b4a5
Alice Fage <afage@linz.govt.nz>
2026-07-02 13:29:15 +1200

    fix: import path

--- packages/topo-imagery-gdal/test/tile/tile_index_test.py
+++ packages/topo-imagery-gdal/test/tile/tile_index_test.py
@@ -1,6 +1,6 @@
 import pytest
 from pytest_subtests import SubTests
-from tile.tile_index_data import MAP_SHEET_DATA
+from tile_index_data import MAP_SHEET_DATA
 from topo_imagery_gdal.tile.tile_index import Bounds, Point, Size, get_bounds_from_name, get_mapsheet_offset, get_tile_offset
 
 
