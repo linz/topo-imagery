50673860b522a1de6b58695d0bcd2dd2793fcee0
Sheetal Poudel <spoudel@linz.govt.nz>
2026-08-06 09:48:01 +1200

    Fixed incorrectly reverted changes TDE-2010

--- packages/topo-imagery-stac/test/imagery/collection_test.py
+++ packages/topo-imagery-stac/test/imagery/collection_test.py
@@ -25,8 +25,8 @@ from topo_imagery_stac.util.stac_extensions import StacExtensions
 
 
 def test_metadata_initialised(fake_collection_context: CollectionContext, subtests: SubTests) -> None:
-    fake_collection_context.event_name = "Forest Assessment"
-    fake_collection_context.geographic_description = "Hawke's Bay Forest Assessment"
+    fake_collection_context.event_name = "Cyclone Gabrielle"
+    fake_collection_context.geographic_description = "Hawke's Bay Cyclone Gabrielle"
     collection = ImageryCollection(fake_collection_context, any_epoch_datetime_string(), any_epoch_datetime_string())
 
     with subtests.test():
@@ -36,10 +36,10 @@ def test_metadata_initialised(fake_collection_context: CollectionContext, subtes
         assert collection.stac["linz:region"] == "hawkes-bay"
 
     with subtests.test():
-        assert collection.stac["linz:geographic_description"] == "Hawke's Bay Forest Assessment"
+        assert collection.stac["linz:geographic_description"] == "Hawke's Bay Cyclone Gabrielle"
 
     with subtests.test():
-        assert collection.stac["linz:event_name"] == "Forest Assessment"
+        assert collection.stac["linz:event_name"] == "Cyclone Gabrielle"
 
     with subtests.test():
         assert collection.stac["linz:lifecycle"] == "completed"
@@ -493,11 +493,11 @@ published as a record of the Cyclone Gabrielle event.",
                 linz_slug=fake_linz_slug(),
                 gsd=Decimal("0.3"),
                 collection_id="a-random-collection-id",
-                event_name="Forest Assessment",
+                event_name="Cyclone Gabrielle",
             ),
-            "Hawke's Bay Forest Assessment 0.3m Aerial Photos (2023)",
+            "Hawke's Bay Cyclone Gabrielle 0.3m Aerial Photos (2023)",
             "Orthophotography within the Hawke's Bay region captured in the 2023 flying season, "
-            "published as a record of the Forest Assessment event.",
+            "published as a record of the Cyclone Gabrielle event.",
             id="Ancillary Aerial Photos with Event",
         ),
         param(
@@ -509,11 +509,11 @@ published as a record of the Cyclone Gabrielle event.",
                 linz_slug=fake_linz_slug(),
                 gsd=Decimal("0.3"),
                 collection_id="a-random-collection-id",
-                event_name="Forest Assessment",
+                event_name="Cyclone Gabrielle",
             ),
-            "Hawke's Bay Forest Assessment 0.3m Near-Infrared Aerial Photos (2023)",
+            "Hawke's Bay Cyclone Gabrielle 0.3m Near-Infrared Aerial Photos (2023)",
             "Near-infrared orthophotography within the Hawke's Bay region captured in the 2023 flying season, "
-            "published as a record of the Forest Assessment event.",
+            "published as a record of the Cyclone Gabrielle event.",
             id="Ancillary Near-Infrared Aerial Photos with Event",
         ),
     ],
@@ -905,15 +905,15 @@ def test_should_not_add_capture_area(
 
 
 def test_event_name_is_present(fake_collection_context: CollectionContext) -> None:
-    fake_collection_context.event_name = "Forest Assessment"
+    fake_collection_context.event_name = "Cyclone Gabrielle"
     collection = ImageryCollection(fake_collection_context, any_epoch_datetime_string(), any_epoch_datetime_string())
-    assert "Forest Assessment" == collection.stac["linz:event_name"]
+    assert "Cyclone Gabrielle" == collection.stac["linz:event_name"]
 
 
 def test_geographic_description_is_present(fake_collection_context: CollectionContext) -> None:
-    fake_collection_context.geographic_description = "Hawke's Bay Forest Assessment"
+    fake_collection_context.geographic_description = "Hawke's Bay Cyclone Gabrielle"
     collection = ImageryCollection(fake_collection_context, any_epoch_datetime_string(), any_epoch_datetime_string())
-    assert "Hawke's Bay Forest Assessment" == collection.stac["linz:geographic_description"]
+    assert "Hawke's Bay Cyclone Gabrielle" == collection.stac["linz:geographic_description"]
 
 
 def test_linz_slug_is_present(fake_collection_context: CollectionContext) -> None:
@@ -1062,8 +1062,8 @@ def test_add_providers_roles_order_sorted(fake_collection_context: CollectionCon
 
 
 def test_update_metadata(fake_collection_context: CollectionContext, subtests: SubTests) -> None:
-    fake_collection_context.event_name = "Forest Assessment"
-    fake_collection_context.geographic_description = "Hawke's Bay Forest Assessment"
+    fake_collection_context.event_name = "Cyclone Gabrielle"
+    fake_collection_context.geographic_description = "Hawke's Bay Cyclone Gabrielle"
     collection = ImageryCollection(fake_collection_context, any_epoch_datetime_string(), any_epoch_datetime_string())
     old_slug = collection.stac["linz:slug"]
     new_metadata = CollectionContext(
