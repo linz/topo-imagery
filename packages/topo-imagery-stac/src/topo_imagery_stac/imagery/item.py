f83abdf44cc6dfd81746aa1b19424951d46b78a6
Alice Fage <afage@linz.govt.nz>
2026-09-09 09:19:29 +1200

    refactor: use stricter types for bbox and geometry TDE-2046

--- packages/topo-imagery-stac/src/topo_imagery_stac/imagery/item.py
+++ packages/topo-imagery-stac/src/topo_imagery_stac/imagery/item.py
@@ -2,7 +2,7 @@ import json
 from typing import Any, TypedDict
 
 from topo_imagery_common.files.fs import read
-from topo_imagery_common.geometry import GeojsonPolygon
+from topo_imagery_common.geometry import BoundingBox, GeojsonPolygon
 from topo_imagery_stac.link import Link, Relation
 from topo_imagery_stac.util.STAC_VERSION import STAC_VERSION
 from topo_imagery_stac.util.media_type import StacMediaType
@@ -94,7 +94,7 @@ class ImageryItem:
         self.stac["properties"]["end_datetime"] = end_datetime
         self.stac["properties"]["datetime"] = None
 
-    def update_spatial(self, geometry: GeojsonPolygon, bbox: tuple[float, ...]) -> None:
+    def update_spatial(self, geometry: GeojsonPolygon, bbox: BoundingBox) -> None:
         """Update the `geometry` and `bbox` (bounding box) of the Item.
 
         Args:
