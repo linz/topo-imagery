6fe15af1c0d65696399710064dec5f2a3e76b911
Alice Fage <afage@linz.govt.nz>
2026-09-10 11:55:07 +1200

    test: fix inverted bbox

--- packages/topo-imagery-stac/test/collection_from_items_test.py
+++ packages/topo-imagery-stac/test/collection_from_items_test.py
@@ -35,7 +35,7 @@ def setup() -> Iterator[ImageryItem]:
         "type": "Polygon",
         "coordinates": [[[1799667.5, 5815977.0], [1800422.5, 5815977.0], [1800422.5, 5814986.0], [1799667.5, 5814986.0]]],
     }
-    bbox = (1799667.5, 5815977.0, 1800422.5, 5814986.0)
+    bbox = (1799667.5, 5814986.0, 1800422.5, 5815977.0)
     start_datetime = "2021-01-27T11:00:00Z"
     end_datetime = "2021-01-27T11:00:00Z"
     item.update_spatial(geometry, bbox)
