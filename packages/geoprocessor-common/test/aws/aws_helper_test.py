fb10f181a9d854b6a55e33dacb68a5b090c83aca
Alice Fage <afage@linz.govt.nz>
2026-09-16 08:01:38 +1200

    refactor: rename packages to geoprocessor

---
+++ packages/geoprocessor-common/test/aws/aws_helper_test.py
@@ -0,0 +1,20 @@
+from geoprocessor_common.aws.aws_helper import parse_path
+from pytest_subtests import SubTests
+
+
+def test_parse_path_s3(subtests: SubTests) -> None:
+    s3_path = "s3://bucket-name/path/to/the/file.test"
+    bucket, key = parse_path(s3_path)
+
+    with subtests.test():
+        assert bucket == "bucket-name"
+
+    with subtests.test():
+        assert key == "path/to/the/file.test"
+
+
+def test_parse_path_local() -> None:
+    local_path = "/home/tmp/file.test"
+    _, file_path = parse_path(local_path)
+
+    assert file_path == "/home/tmp/file.test"
---
+++ packages/geoprocessor-common/test/files/fs_local_test.py
@@ -0,0 +1,44 @@
+import os
+
+import pytest
+from geoprocessor_common.files.fs_local import exists, read, write
+
+
+@pytest.mark.dependency(name="write")
+def test_write(setup: str) -> None:
+    target = setup
+    path = os.path.join(target, "test.file")
+    write(path, b"test")
+    assert os.path.isfile(path)
+
+
+def test_write_non_existing_dir(setup: str) -> None:
+    target = setup
+    path = os.path.join(target, "new_dir/test.file")
+    write(path, b"test")
+    assert os.path.isfile(path)
+
+
+@pytest.mark.dependency(name="read", depends=["write"])
+def test_read(setup: str) -> None:
+    content = b"test content"
+    target = setup
+    path = os.path.join(target, "test.file")
+    write(path, content)
+    file_content = read(path)
+    assert file_content == content
+
+
+@pytest.mark.dependency(name="exists", depends=["write"])
+def test_exists(setup: str) -> None:
+    content = b"test content"
+    target = setup
+    path = os.path.join(target, "test.file")
+    write(path, content)
+    found = exists(path)
+    assert found is True
+
+
+def test_exists_file_not_found() -> None:
+    found = exists("/tmp/test.file")
+    assert found is False
